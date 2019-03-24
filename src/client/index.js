68e30c28ce7285bdac1843e6b70f31fb980bd584
Jun Cho <juncho@Juns-MacBook-Pro.local>
2019-03-24 18:14:49 -0400

    Use wavesurfer instead of howler; ui additions

--- src/client/index.js
+++ src/client/index.js
@@ -1,35 +1,46 @@
-const {Howl, Howler} = require('howler')
+var WaveSurfer = require('wavesurfer')
+var TimelinePlugin = require('./node_modules/wavesurfer/plugin/wavesurfer.timeline.js');
 
+let wavesurfer = null
 
+const status = {
+	playing: false,
+}
 
 function readFile(files) {
-  
-  var fileReader = new FileReader();
-  console.log(files, files.target.baseURI)
-  // fileReader.readAsArrayBuffer(files[0]);
-  // fileReader.onload = function(e) {
-    // console.log(("Filename: '" + files[0].name + "'"), ( "(" + ((Math.floor(files[0].size/1024/1024*100))/100) + " MB)" ));
-  // }
-  var sound = new Howl({
-	  src: ["violin-cut.wav"],
-	  autoplay: true,
-  loop: true,
-  volume: 0.5,
-  onend: function() {
-    console.log('Finished!');
-  }
-	});
+	var fileReader = new FileReader();
 
-	sound.play();
-  
-  console.log('hi')
+	wavesurfer = WaveSurfer.create({
+	    container: '#waveform',
+	});
+	WaveSurfer.Timeline.init({
+		wavesurfer: wavesurfer,
+		container: "#wave-timeline"
+	});
+	// Testing!
+	wavesurfer.load('violin-cut.wav');
+	wavesurfer.on('ready', function () {
+		console.log('yes!')
+	})
 }
 
 window.addEventListener("load", 
 	function(){
-		var button = document.getElementById('audioFileChooser')
-		console.log(button)
-		console.log('wowooowowo')
-		button.onchange = readFile
+		// Initialize everything!
+		var uploadButton = document.getElementById('audioFileChooser')
+		uploadButton.onchange = readFile
+
+		var playPauseButton = document.getElementById('button_play_pause')
+		playPauseButton.onclick = () => { wavesurfer.playPause() }
+
+		var progress = document.getElementById('menu-bar_progress')
+		setInterval(
+			() => { 
+				if (wavesurfer.isPlaying()) {
+					progress.innerHTML = `${wavesurfer.getCurrentTime()}` 
+				}
+			},
+			100)
+
 	}
 )
