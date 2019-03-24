0d44ee4f7c39963306f20f58da0c8fc2bc9eef37
Jun Cho <juncho@Juns-MacBook-Pro.local>
2019-03-24 18:49:05 -0400

    client: add inputs, and ability to add

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ var WaveSurfer = require('wavesurfer')
 var TimelinePlugin = require('./node_modules/wavesurfer/plugin/wavesurfer.timeline.js');
 
 let wavesurfer = null
+let numInputs = 1
 
 const status = {
 	playing: false,
@@ -24,6 +25,35 @@ function readFile(files) {
 	})
 }
 
+function addInput(num) {
+	console.log('hi', num)
+	var inputLine = document.createElement("tr");
+	var cell = document.createElement("td");
+
+	var aInput = document.createElement("input")
+	var bInput = document.createElement("input")
+	var pitchInput = document.createElement("input")
+
+	aInput.id = `inputs_a_${num}`
+	bInput.id = `inputs_b_${num}`
+	pitchInput.id = `inputs_pitch_${num}`
+
+	var inputsTable = document.getElementById('inputs_table')
+	var row = inputsTable.insertRow(num)
+	var aCell = row.insertCell(0)
+	var bCell = row.insertCell(1)
+	var pitchCell = row.insertCell(2)
+
+	aCell.appendChild(aInput)
+	bCell.appendChild(bInput)
+	pitchCell.appendChild(pitchInput)
+	console.log('set table')
+}
+
+function removeInput(num) {
+	// TODO
+}
+
 window.addEventListener("load", 
 	function(){
 		// Initialize everything!
@@ -36,11 +66,14 @@ window.addEventListener("load",
 		var progress = document.getElementById('menu-bar_progress')
 		setInterval(
 			() => { 
-				if (wavesurfer.isPlaying()) {
+				if (wavesurfer && wavesurfer.isPlaying()) {
 					progress.innerHTML = `${wavesurfer.getCurrentTime()}` 
 				}
 			},
 			100)
 
+		var addInputButton = document.getElementById('inputs_add')
+		addInputButton.onclick = () => { addInput(numInputs + 1)}
+		console.log('done binding', addInputButton)
 	}
 )
