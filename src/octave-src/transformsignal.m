f302921338dacb5382ed72ebe062163a7fa0d80e
Suraj Amarnani <surajamarnani@hotmail.com>
2019-03-25 13:39:59 -0400

    fix EOL problem

--- src/octave-src/transformsignal.m
+++ src/octave-src/transformsignal.m
@@ -1,1 +1,17 @@
-function tsig = transformsignal(x, fs, f0_est, ranges, correct_notes)  notes = getnotes();  tsig = x;  ranges  correct_notes  for i = 1:size(ranges)(1)    l = ranges(i,1) * fs;    r = ranges(i,2) * fs;    desired_freq = getfield(notes, correct_notes{1, i});    avg_freq = mean(f0_est(l:r));    frac = desired_freq / avg_freq;    corrected = correctsingle(x(l:r), frac, 0.0000001);  endforendfunction
+function tsig = transformsignal(x, fs, f0_est, ranges, correct_notes)
+  notes = getnotes();
+  tsig = x;
+  ranges
+  correct_notes
+  for i = 1:size(ranges)(1)
+    l = ranges(i,1) * fs;
+    r = ranges(i,2) * fs;
+    desired_freq = getfield(notes, correct_notes{1, i});
+    avg_freq = mean(f0_est(l:r));
+    frac = desired_freq / avg_freq;
+    corrected = correctsingle(x(l:r), frac, 0.0000001);
+  endfor
+endfunction
+
+
+
