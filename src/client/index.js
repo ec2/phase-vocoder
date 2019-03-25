3f0be3a015c7f24662622e18c24edfa305d53f51
Jun Cho <juncho@Juns-MacBook-Pro.local>
2019-03-24 20:02:22 -0400

    client: form data

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 var WaveSurfer = require('wavesurfer')
 var TimelinePlugin = require('./node_modules/wavesurfer/plugin/wavesurfer.timeline.js');
 
+var uuidv4 = require('uuid/v4')
+
 let wavesurfer = null
 let numInputs = 0
 
@@ -34,6 +36,8 @@ function addInput(num) {
 	var bInput = document.createElement("input")
 	var pitchInput = document.createElement("input")
 
+	pitchInput.pattern= "[A-Ga-g][1-9](#|B|b)?"
+
 	var removeButton = document.createElement('button')
 	removeButton.onclick = removeInput
 	removeButton.id = `inputs_remove_${num}`
@@ -57,14 +61,36 @@ function addInput(num) {
 	pitchCell.appendChild(pitchInput)
 	removeButtonCell.appendChild(removeButton)
 	console.log('set table')
+	numInputs += 1
 }
 
-function removeInput(target) {
-	// TODO
-	console.log('removeInput', target, target.target.id)
-	var rowNum = target.target.id.split('_')[2]
+function removeInput(event) {
+	var rowNum = event.target.id.split('_')[2]
 	var toRemove = document.getElementById(`inputs_row_${rowNum}`)
 	toRemove.remove()
+}
+
+function submit(event) {
+	const len = event.target.length
+	var pitch_shifts = []
+	for (var i = 0; i < len; i++) {
+		i += 1
+		if (i + 2 >= len) break // lmao
+		var shift = {
+			'start_time': event.target[i].value,
+			'end_time': event.target[i+1].value,
+			'desired_note': event.target[i+2].value,
+		}
+		pitch_shifts.push(shift)
+		i += 2
+	}
+	const payload = {
+		'id': uuidv4(),
+		pitch_shifts,
+	}
+
+	// don't submit for now TODO
+	event.preventDefault()
 }
 
 window.addEventListener("load", 
@@ -92,5 +118,9 @@ window.addEventListener("load",
 		// Add inputs listener
 		var addInputButton = document.getElementById('inputs_add')
 		addInputButton.onclick = () => { addInput(numInputs + 1)}
+
+		// Set up the form submission
+		const form = document.getElementById('form');
+		form.onsubmit = submit;
 	}
 )
