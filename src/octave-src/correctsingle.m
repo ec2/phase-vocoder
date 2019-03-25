09533292ae263a80aa9472e7ec1ed1a774250bea
Suraj Amarnani <surajamarnani@v1040-wn-rt-c-110-201.campus-dynamic.uwaterloo.ca>
2019-03-25 13:35:57 -0400

    octave 2

--- src/octave-src/correctsingle.m
+++ src/octave-src/correctsingle.m
@@ -1,1 +1,12 @@
-function tsig = correctsingle(x, factor, decimal)  pkg load signal  e = pvoc(x, factor);  [p,q] = rat(length(x)/length(e), decimal);  tsig = resample(e, p, q);endfunction
+function tsig = correctsingle(x, factor, decimal)
+  pkg load signal
+  e = pvoc(x, factor);
+  [p,q] = rat(length(x)/length(e), decimal);
+  tsig = resample(e, p, q);
+  length(tsig)
+  if(length(tsig) > length(x))
+    tsig = tsig(1:length(x));
+  elseif(length(tsig) < length(x))
+    tsig = horzcat(tsig, x(length(tsig):end));
+  endif
+endfunction
