6d66eae89e712b1b9962b68172ed76d0b2754760
Eric Tu <tu.eric@hotmail.com>
2019-03-27 12:54:45 -0400

    remove unnecessary libs

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser')
-const url = require
 const fs = require('fs');
-const path = require('path');
 const timeout = require('connect-timeout'); //express v4
 
 const app = express();
