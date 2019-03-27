5d173df216fa114d9a679ee6cae6c88094135bb4
Jun Cho <juncho@Juns-MacBook-Pro.local>
2019-03-26 22:59:58 -0400

    Convert line endings to unix

--- src/octave-src/run.m
+++ src/octave-src/run.m
@@ -1,1 +1,5 @@
-x = wavread('toneb3.wav');out = transformsignal(x, [1, length(x)], [.5]);wavwrite(out, 'out.wav');
+x = wavread('toneb3.wav');
+
+out = transformsignal(x, [1, length(x)], [.5]);
+wavwrite(out, 'out.wav');
+
