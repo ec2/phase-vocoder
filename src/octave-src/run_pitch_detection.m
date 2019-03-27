f20891c11e4ee1849161dcf697c383ce97e7ea9b
Suraj Amarnani <surajamarnani@v1040-wn-rt-c-65-99.campus-dynamic.uwaterloo.ca>
2019-03-27 13:16:43 -0400

    fix 0 start time bug

--- src/octave-src/run_pitch_detection.m
+++ src/octave-src/run_pitch_detection.m
@@ -9,5 +9,5 @@ f0_est = getpitch(x, fs);
 f0_est_ds = decimate(f0_est, 10);
 filename_ds=sprintf('/tmp/%s/%s.csv', uid, uid);  
 filename_orig=sprintf('/tmp/%s/%s_orig.csv', uid, uid);  
-csvwrite(filename_ds, f0_est_ds)
-csvwrite(filename_orig, f0_est)
+csvwrite(filename_ds, f0_est_ds);
+csvwrite(filename_orig, f0_est);
--- src/octave-src/transformsignal.m
+++ src/octave-src/transformsignal.m
@@ -4,8 +4,8 @@ function tsig = transformsignal(x, fs, f0_est, ranges, correct_notes)
   ranges
   correct_notes
   for i = 1:size(ranges)(1)
-    l = ranges(i,1) * fs;
-    r = ranges(i,2) * fs;
+    l = ranges(i,1) * fs + 1;
+    r = ranges(i,2) * fs + 1;
     desired_freq = getfield(notes, correct_notes{1, i});
     avg_freq = mean(f0_est(l:r));
     frac = desired_freq / avg_freq;
