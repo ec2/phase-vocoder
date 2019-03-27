8a28f23be808653935ba57840e3bac5f074b3c0b
Suraj Amarnani <surajamarnani@v1040-wn-rt-c-65-99.campus-dynamic.uwaterloo.ca>
2019-03-27 12:19:20 -0400

    downsample semicolon

--- src/octave-src/run_pitch_detection.m
+++ src/octave-src/run_pitch_detection.m
@@ -6,6 +6,6 @@ uid = argv(){1};
 filename = sprintf("/tmp/%s/%s.wav", uid, uid);
 [x, fs] = audioread(filename);
 f0_est = getpitch(x, fs);
-f0_est = decimate(f0_est, 10)
+f0_est = decimate(f0_est, 10);
 filename=sprintf('/tmp/%s/%s.csv', uid, uid);    
 csvwrite(filename, f0_est)
