39d2abc1b64953224e73838098f415f90d40af39
Eric Tu <tu.eric@hotmail.com>
2019-03-27 12:35:59 -0400

    pitch shift works

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const bodyParser = require ('body-parser')
+const bodyParser = require('body-parser')
 const url = require
 const fs = require('fs');
 const path = require('path');
+const timeout = require('connect-timeout'); //express v4
 
 const app = express();
 
@@ -16,9 +17,18 @@ app.use((request, response, next) => {
     next()
 })
 
+app.use(timeout(1200000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next){
+  if (!req.timedout) next();
+}
+
+
+
 app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -32,43 +42,67 @@ app.get('/', (request, response) => {
 
 console.log(__dirname)
 
-app.post('/detect_pitch', (req, res) =>{        
+app.post('/detect_pitch', (req, res) => {
     if (Object.keys(req.files).length == 0) {
         return res.status(400).send('No files were uploaded.');
     }
     console.log("REQ FILES ", req.files)
     let sampleFile = req.files.sampleFile;
     let uid = sampleFile.md5
+    let dirPath = `/tmp/${uid}`;
+    let filePath = dirPath + `/${uid}.wav`;
+
+    let dirPathExists = fs.existsSync(dirPath);
+    let filePathExists = fs.existsSync(filePath);
 
     // Saves uploaded file into /tmp/<UID>/
-    fs.mkdirSync(`/tmp/${uid}`)
-    sampleFile.mv(`/tmp/${uid}/${uid}.wav`, function(err) {
-        if (err){
-            console.log(err)
-            return res.status(500).send(err);
-        }
-        // run the octave script in a shell
-        dir = exec(`cd /root/phase-vocoder/src/octave-src/ && /root/phase-vocoder/src/octave-src/run_pitch_detection.m ${uid}`, function (err, stdout, stderr) {
+    if (!dirPathExists) {
+        fs.mkdirSync(dirPath)
+    }
+    if (filePathExists) {
+        fs.readFile(`/tmp/${uid}/${uid}.csv`, (err, data) => {
             if (err) {
-                console.log (err)
-                return res.status(666).send(err);
+                console.log(err)
+                return res.send(667).send(err);
             }
-            // reads the csv file to be returned. 
-            fs.readFile(`/tmp/${uid}/${uid}.csv`, (err, data) => {
-                if(err){
+            const returnObj = {
+                code: 200,
+                pitch: data.toString(),
+                uid: uid
+            }
+            console.log("response!")
+            res.send(JSON.stringify(returnObj))
+        });
+    } else {
+
+        sampleFile.mv(filePath, function (err) {
+            if (err) {
+                console.log(err)
+                return res.status(500).send(err);
+            }
+            // run the octave script in a shell
+            dir = exec(`cd /root/phase-vocoder/src/octave-src/ && /root/phase-vocoder/src/octave-src/run_pitch_detection.m ${uid}`, function (err, stdout, stderr) {
+                if (err) {
                     console.log(err)
-                    return res.send(667).send(err);
+                    return res.status(666).send(err);
                 }
-                const returnObj = {
-                    code: 200,
-                    pitch: data.toString(),
-                    uid: uid
-                }
-                console.log("response!")
-                res.send (JSON.stringify(returnObj))
+                // reads the csv file to be returned. 
+                fs.readFile(`/tmp/${uid}/${uid}.csv`, (err, data) => {
+                    if (err) {
+                        console.log(err)
+                        return res.send(667).send(err);
+                    }
+                    const returnObj = {
+                        code: 200,
+                        pitch: data.toString(),
+                        uid: uid
+                    }
+                    console.log("response!")
+                    res.send(JSON.stringify(returnObj))
+                });
             });
         });
-    });
+    }
 });
 
 /*
@@ -86,7 +120,7 @@ app.post('/correct_pitch', (req, res) => {
 
     console.log(pitch_shifts)
 
-    for (let i = 0 ; i < pitch_shifts.length ; i ++){
+    for (let i = 0; i < pitch_shifts.length; i++) {
         const start = pitch_shifts[i].start_time;
         const end = pitch_shifts[i].end_time;
         const note = pitch_shifts[i].desired_note;
@@ -98,14 +132,12 @@ app.post('/correct_pitch', (req, res) => {
 
     dir = exec(`cd /root/phase-vocoder/src/octave-src/ && /root/phase-vocoder/src/octave-src/run_pitch_shift.m ${uid} ${paramStr}`, function (err, stdout, stderr) {
         if (err) {
-            console.log (err)
+            console.log("Error: ", err)
             return res.status(666).send(err);
         }
-        res.send()
+        console.log('Done Shifting')
+        res.download(`/tmp/${uid}/${uid}_shifted.wav`)
     });
-    res.send("all gucci")
-
-
 });
 
 app.listen(3000);
