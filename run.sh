fb20697e176688acc6ff1d5c4257f32d3e3784ec
Eric Tu <tu.eric@hotmail.com>
2019-04-01 19:32:08 -0400

    run script will stop docker container when exiting electron app

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
-docker run -d -p 3000:3000 -v $PWD:/root/phase-vocoder tueric/phase-vocoder  
+docker run -d -p 3000:3000 -v $PWD:/root/phase-vocoder --cidfile="dockercid" tueric/phase-vocoder  
 cd src/client/
 npm start 
+
 cd -
+
+CID=$(cat dockercid)
+docker stop $CID
+rm dockercid
